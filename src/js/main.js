6420c8ecd74121d8af40159b92a73cc1fe7d1f1b
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-09-04 22:26:31 +0200

    problemas con foun product

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 console.log('>> Ready? Yes, Ready :)');
 
 let products = [];
+let cart = [];
 
 const getApiData = () => {
     fetch('https://fakestoreapi.com/products')
@@ -13,16 +14,21 @@ const getApiData = () => {
     })
 };
 
+document.querySelector(".products").classList.add("js-products");
 const productsElement = document.querySelector('.js-products');
+
+document.querySelector(".shopping_bag").classList.add("js-shopping_bag");
+const shoppingBag = document.querySelector("js-shopping_bag");
+
 
 const renderProducts = product => {
     let htmlCode = '';
     htmlCode += `<article class="card">`;
     htmlCode += `<img src="${product.image}" class="card_img" alt="Producto: ${product.title}">`;
     htmlCode += `<h3 class="card__title">${product.title}</h3>`;
-    htmlCode += `<p class="card__description">${product.description}</h3>`;
-    htmlCode += `<p class="card__price">${product.price}</h3>`;
-    htmlCode += `<button class="card__button">Comprar</button>`;
+    htmlCode += `<p class="card__description">${product.description}</p>`;
+    htmlCode += `<p class="card__price">${product.price}</p>`;
+    htmlCode += `<button class="js-add-products" data-id="${product.id}">Comprar</button>`;
     htmlCode += `</article>`;
     return htmlCode;
 };
@@ -33,6 +39,8 @@ const renderProductsList = () => {
         productsCode += renderProducts(product);
     }
     productsElement.innerHTML = productsCode;
+
+    listenAddProductsButtons();
 };
 getApiData();
 
@@ -42,7 +50,6 @@ const input = document.querySelector(".js_in_search");
 const handleClickSearchButton = (event) => {
   event.preventDefault();
   const inputValue = input.value.toLowerCase();
-  console.log("Haz buscado:", inputValue);
   const searchResults = products.filter (p => 
     p.title.toLowerCase().includes(inputValue) ||
     p.description.toLowerCase().includes(inputValue)
@@ -53,6 +60,61 @@ const handleClickSearchButton = (event) => {
   }
 };
 searchButton.addEventListener("click", handleClickSearchButton);
+
+const listenAddProductsButtons = () => {
+    const productsButtons = document.querySelectorAll('.js-add-products');
+    for (const productsButton of productsButtons) {
+        productsButton.addEventListener('click', addProduct);
+    }
+}
+
+const getShoppingBagHtmlCode = item => {
+    let htmlCode = '';
+    htmlCode += `<article class="card">`;
+    htmlCode += `<img src="${product.image}" class="card_img" alt="Producto: ${product.title}">`;
+    htmlCode += `<h3 class="card__title">${product.title}</h3>`;
+    htmlCode += `<p class="card__description">${product.description}</p>`;
+    htmlCode += `<p class="card__price">${product.price}</p>`;
+    htmlCode += `<button class="js-add-products" data-id="${product.id}">Comprar</button>`;
+    htmlCode += `</article>`;
+    return htmlCode;
+}
+
+const addProduct = (event) => {
+    console.log("han clicado en un producto", event.target.dataset.id);
+    const clickedId = event.target.dataset.id;
+    const foundProduct = products.find((item) => item.id === clickedId);
+    console.log(foundProduct);
+    cart.push({
+        img: foundProduct.image,
+        title: foundProduct.title,
+        description: foundProduct.description,
+        price: foundProduct.price,
+    })
+    paintCartItems();
+}
+
+const paintCartItems = () => {
+    cartElements.innerHTML = '';
+    for (const item of cart) {
+        cartElement.innerHTML += getCartItemHtmlCode(item);
+    }
+    cartElement.innerHTML += getCartTotalHtmlCode();
+}
+
+productsElement.addEventListener("click", (event) => {
+    if (event.target.classList.contains ("js-add-products")) {
+
+        event.target.classList.toggle("_delete");
+        
+        if (event.target.classList.contains("_delete")) {
+            event.target.textContent = "Eliminar"
+        } else {
+            event.target.textContent = "Comprar";
+    }
+  }
+});
+
 
 //    "id": 1,
 //    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
